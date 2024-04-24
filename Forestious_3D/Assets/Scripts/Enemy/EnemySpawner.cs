8ceee46720e5eaf33b21038268bf07d021657596
integeek <ambre.giraudlanza@outlook.fr>
2024-04-24 12:49:35 +0300

    retirer commentaire

--- Forestious_3D/Assets/Scripts/Enemy/EnemySpawner.cs
+++ Forestious_3D/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -5,9 +5,9 @@ public class EnemySpawner : MonoBehaviour
     public GameObject enemyPrefab; 
     public int minEnemies = 2;
     public int maxEnemies = 4;
-    public float spawnRadius = 5f; // Rayon autour du point central pour le spawn des ennemis
+    public float spawnRadius = 5f;
     public LayerMask groundLayer;
-    public GameObject centerObject; // GameObject au milieu de l'hexagone
+    public GameObject centerObject;
 
     void Start()
     {
@@ -27,14 +27,11 @@ public class EnemySpawner : MonoBehaviour
 
     Vector3 GenerateSpawnPosition()
     {
-        // Obtenez la position du GameObject au milieu de l'hexagone
         Vector3 centerPosition = centerObject.transform.position;
 
-        // Générez une position aléatoire dans un rayon autour du point central
         Vector2 randomPoint = Random.insideUnitCircle * spawnRadius;
         Vector3 spawnPosition = new Vector3(randomPoint.x, 1.755f, randomPoint.y) + centerPosition;
 
-        // Effectuez un raycast vers le bas pour s'assurer que la position de spawn est sur le sol
         RaycastHit hit;
         if (Physics.Raycast(spawnPosition, Vector3.down, out hit, Mathf.Infinity, groundLayer))
         {
--- Forestious_3D/Assets/Scripts/GestionSalle.cs
+++ Forestious_3D/Assets/Scripts/GestionSalle.cs
@@ -4,30 +4,26 @@ public class GestionSalle : MonoBehaviour
 {
     public Camera mainCamera;
     public EnemySpawner enemySpawner;
-    private bool enemiesSpawned = false; // Booléen pour garder une trace du statut de spawn des ennemis
-    public GameObject centreObject; // Le gameObject centre défini dans l'éditeur Unity
+    private bool enemiesSpawned = false;
+    public GameObject centreObject;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !enemiesSpawned) // Vérifiez si les ennemis n'ont pas encore été spawnés
+        if (other.CompareTag("Player") && !enemiesSpawned)
         {
-            // Obtenez les coordonnées du gameObject centre
+  
             Vector3 centrePosition = centreObject.transform.position;
 
-            // Mettez la caméra 21 au-dessus du gameObject centre
             Vector3 newPos = new Vector3(centrePosition.x, centrePosition.y + 21f, centrePosition.z);
             mainCamera.transform.position = newPos;
 
-            // Spawn les ennemis
             enemySpawner.SpawnEnemies();
-            enemiesSpawned = true; // Mettez à jour le statut de spawn des ennemis
+            enemiesSpawned = true;
         }
-        else if (other.CompareTag("Player")) // Si les ennemis ont déjà été spawnés, changez simplement la position de la caméra
+        else if (other.CompareTag("Player"))
         {
-            // Obtenez les coordonnées du gameObject centre
             Vector3 centrePosition = centreObject.transform.position;
 
-            // Mettez la caméra 21 au-dessus du gameObject centre
             Vector3 newPos = new Vector3(centrePosition.x, centrePosition.y + 21f, centrePosition.z);
             mainCamera.transform.position = newPos;
         }
